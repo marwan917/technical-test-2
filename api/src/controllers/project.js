eb4f00fe54ab410c8575cbeb817ee1d919a92fb3
NAWEL <nawel@MacBook-Air-de-NAWEL.local>
2023-09-29 15:57:09 +0200

    ADD modification marwan api

--- api/src/controllers/project.js
+++ api/src/controllers/project.js
@@ -19,7 +19,7 @@ router.get("/list", passport.authenticate("user", { session: false }), async (re
 
 router.get("/:id", passport.authenticate("user", { session: false }), async (req, res) => {
   try {
-    const data = await ProjectObject.find({ _id: req.params.id });
+    const data = await ProjectObject.findOne({ _id: req.params.id });
     return res.status(200).send({ ok: true, data });
   } catch (error) {
     console.log(error);
@@ -34,6 +34,8 @@ router.post("/", passport.authenticate("user", { session: false }), async (req, 
   } catch (error) {
     if (error.code === 11000) return res.status(409).send({ ok: false, code: PROJECT_ALREADY_EXISTS });
     console.log(error);
+    
+    
     return res.status(500).send({ ok: false, code: SERVER_ERROR });
   }
 });
